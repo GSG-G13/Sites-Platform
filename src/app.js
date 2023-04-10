f67a782202304614825554ded973d99fac0e390f
raghdarizq <raghdarizq2@gmail.com>
2023-04-10 21:10:34 +0300

    Relates #3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const cookieParser = require('cookie-parser');
 const { join } = require('path');
 const router = require('./router');
 app.use(express.json());

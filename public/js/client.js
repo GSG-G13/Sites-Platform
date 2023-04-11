c6bf7b2fc6bd37ee1184727d4dac907e4fc266e3
Aya Qunoo <qunooa@gmail.com>
2023-04-11 22:21:06 +0200

    handle data Realates #16

---
+++ public/js/client.js
@@ -0,0 +1,80 @@
+const main = document.querySelector('.main');
+// const searchBar = document.querySelector("#search-form").querySelector("input");
+const createPost = (data) => {
+    const container = document.createElement('div');
+    container.classList.add('friend_post');
+
+
+    const friendPostTop = document.createElement('div');
+    friendPostTop.classList.add('friend_post_top');
+
+
+    const imgAndName = document.createElement('div');
+    imgAndName.classList.add('img_and_name');
+
+    const img = document.createElement('img');
+    img.src = data.photo;
+
+
+    const friendsName = document.createElement('div');
+    friendsName.classList.add('friends_name');
+
+    const friendsNamePara = document.createElement('p');
+    friendsNamePara.classList.add('friends_name');
+    friendsNamePara.textContent = data.username;
+
+
+    const timePara = document.createElement('p');
+    timePara.classList.add('time');
+    timePara.textContent = data.created_at;
+    const userGroupIcon = document.createElement('i');
+    userGroupIcon.classList.add('fa-solid', 'fa-user-group');
+    timePara.appendChild(userGroupIcon);
+
+
+    friendsName.appendChild(friendsNamePara);
+    friendsName.appendChild(timePara);
+
+    imgAndName.appendChild(img);
+    imgAndName.appendChild(friendsName);
+    const title = document.createElement('p');
+    title.textContent = data.title;
+
+
+    const img2 = document.createElement('img');
+    img2.src = data.photo_website;
+
+
+    const info = document.createElement('div');
+    info.classList.add('info');
+
+
+    const description = document.createElement('p');
+    description.classList.add('description');
+    description.textContent = data.description
+
+    friendPostTop.appendChild(imgAndName);
+    container.appendChild(friendPostTop);
+    container.appendChild(title);
+    container.appendChild(img2);
+    info.appendChild(description);
+    container.appendChild(info);
+
+    main.appendChild(container);
+
+
+}
+
+fetch('/users/posts').then(res => res.json()).then(data => data.forEach(element => {
+    creatRow(element);
+}));
+
+
+
+// const searchByUsername=(searchTerm, data)=> {
+//     // Filter the data array for objects with a matching username
+//     const results = data.filter(obj => obj.username.toLowerCase() === searchTerm.toLowerCase());
+    
+//     // Return the results array
+//     return results;
+//   }
--- src/router/post.js
+++ src/router/post.js
@@ -1,7 +1,17 @@
 const homeRouter = require('express').Router();
 const { getPosts } = require('../controller');
-
-
-homeRouter.get('/posts', getPosts)
+const path = require('path');
+// const auth = (req, res, next) => {
+//     const accesstoken = req.cookies.accesstoken;
+//     if (accesstoken) {
+//         next()
+//     } else {
+//         res.send("/users/signinuser")
+//     }
+// }
+homeRouter.get('/posts', getPosts);
+homeRouter.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/html/users/home.html'))
+})
 
 module.exports = homeRouter

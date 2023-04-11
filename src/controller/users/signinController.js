8eefd5cb582aff4058e6ea2e4aee89b7d5890a0d
Nour-Agha99 <temoagha@gmail.com>
2023-04-11 14:18:47 +0300

    anything

--- src/controller/users/signinController.js
+++ src/controller/users/signinController.js
@@ -1,6 +1,7 @@
 const { log } = require('console');
 const { signinQuery } = require('../../database/query');
 const { signinSchema } = require('../../router/schema/user.schema')
+
 const signinController = (req, res) => {
     const { email, password } = req.body;
     const { error, value } = signinSchema.validate({ email, password }, { abortEarly: false })
